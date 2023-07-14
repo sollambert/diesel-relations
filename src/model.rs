7f9c8386dea2c21cc25d90d75f97977c99e64d68
Solomon Lambert <sol.lambert@gmail.com>
2023-07-14 17:08:01 -0500

    add models for authors and books_authors

--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,6 @@
 use diesel::prelude::*;
 
-use crate::schema::{books, pages};
+use crate::schema::{books, pages, authors, books_authors};
 
 #[derive(Queryable, Identifiable, Selectable, Debug, PartialEq)]
 #[diesel(table_name = books)]
@@ -17,4 +17,21 @@ pub struct Page {
     pub page_number: i32,
     pub content: String,
     pub book_id: i32,
+}
+
+#[derive(Queryable, Selectable, Identifiable, PartialEq, Debug)]
+#[diesel(table_name = authors)]
+pub struct Author {
+    pub id: i32,
+    pub name: String,
+}
+
+#[derive(Identifiable, Selectable, Queryable, Associations, Debug)]
+#[diesel(belongs_to(Book))]
+#[diesel(belongs_to(Author))]
+#[diesel(table_name = books_authors)]
+#[diesel(primary_key(book_id, author_id))]
+pub struct BookAuthor {
+    pub book_id: i32,
+    pub author_id: i32,
 }
